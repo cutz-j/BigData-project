3a21bb34c7b843307699c72bed9950a078fda7d3
cutz-j <cutz309@gmail.com>
2018-11-06 22:40:14 +0900

    Update

--- pop_LSTM(predict).py
+++ pop_LSTM(predict).py
@@ -12,6 +12,8 @@ warnings.filterwarnings('ignore')
 tf.set_random_seed(777)
 tf.reset_default_graph()
 
+time = ["%i"%(i) + "-%i"%(3) for i in range(2010, 2022)]
+
 ## parameter ##
 seq_length = 5 # 데이터의 시퀀스 length (연관된 데이터)  -> output row
 data_dim = 1 # 입력 차원 --> 인구수 1 (동별)
@@ -27,7 +29,7 @@ pop_2103 = []
 all_data = pd.read_csv("d:/project_data/peopleDataAll01.csv", sep=",", encoding='cp949')
 
 ## LSTM ##
-for k in range(200,204):
+for k in [-18]:
     tf.reset_default_graph()
     test1 = all_data.iloc[:, [k]] # shape(105,1) m = 105
     keep_prob = tf.placeholder(tf.float32)
@@ -57,11 +59,21 @@ for k in range(200,204):
     
     cell1 = lstm_cell(30)
     cell2 = lstm_cell(20)
+    cells = []
+    cells.append(cell1)
+    cells.append(cell2)
 #    cell3 = rnn.DropoutWrapper(lstm_cell(1), input_keep_prob=keep_prob, output_keep_prob=keep_prob, seed=77)
     
     
     #cell = rnn.BasicLSTMCell(num_units=hidden_size, state_is_tuple=True, activation=tf.tanh)
     cell = rnn.MultiRNNCell([cell1, cell2], state_is_tuple=True) # dropout cell 5개
+    
+    ## tensor board ##
+    for one_lstm_cell in cells:
+        one_kernel = one_lstm_cell.variables
+        tf.summary.histogram("Kernel", one_kernel)
+#        tf.summary.histogram("Bias", one_bias)
+    
     X = tf.placeholder(dtype=tf.float32, shape=[None, seq_length, data_dim])
     y = tf.placeholder(dtype=tf.float32, shape=[None, 1])
     #
@@ -79,14 +91,17 @@ for k in range(200,204):
     predicts = tf.placeholder(tf.float32, [None, 1])
     MSE = tf.sqrt(tf.reduce_mean(tf.square(predicts - targets)))
     
+    summary_op = tf.summary.merge_all()
     ## session ##
     # training#
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
+    train_writer = tf.summary.FileWriter("project_data/logdir/", graph=tf.get_default_graph())
     for i in range(iteration):
-        cost_val, _, out= sess.run([cost, train, output], feed_dict={X: x_train, y: y_train, keep_prob: 0.7})
+        cost_val, _, out, step_summary= sess.run([cost, train, output, summary_op], feed_dict={X: x_train, y: y_train, keep_prob: 0.7})
 #        if i % 100 == 0:
 #            print(cost_val)
+        train_writer.add_summary(step_summary)
 
     # predict # --> 201809 30개월 후 --> 202103
     for t in range(30):
@@ -100,9 +115,12 @@ for k in range(200,204):
     if k % 1 == 0:
         data_concat = mm1.inverse_transform(test1)
         data_concat = pd.DataFrame(data_concat)
-        plt.figure()
+        plt.figure(figsize=(16,8))
         plt.plot(data_concat.iloc[:106, :], 'r-')
         plt.plot(data_concat.iloc[105:, :].index, data_concat.iloc[105:, :], 'b-')
+        plt.xlabel("Time")
+        plt.ylabel("Population")
+        plt.xticks(ticks=np.arange(0, 135, 12), labels=list(time))
         plt.show()
     
     pop_2103.append(int(data_concat.iloc[-1][0]))
@@ -111,4 +129,4 @@ for k in range(200,204):
     
 
 plist = pd.DataFrame(pop_2103).T
-plist.to_csv("d:/project_data/pop_2103.csv")
+#plist.to_csv("d:/project_data/pop_2103.csv")
--- pop_LSTM(training).py
+++ pop_LSTM(training).py
@@ -12,6 +12,9 @@ warnings.filterwarnings('ignore')
 tf.set_random_seed(777)
 tf.reset_default_graph()
 
+
+time = ["%i"%(i) + "-%i"%(j) for i in range(2010, 2022) for j in range(3, 12, 4)]
+
 ## parameter ##
 seq_length = 5 # 데이터의 시퀀스 length (연관된 데이터)  -> output row
 data_dim = 1 # 입력 차원 --> 인구수 1 (동별)
@@ -27,105 +30,107 @@ predict_list = []
 all_data = pd.read_csv("d:/project_data/peopleDataAll01.csv", sep=",", encoding='cp949')
 
 ## 청운효자동 LSTM ##
-with tf.device('GPU:0'):
-    for k in range(len(all_data.columns)-1, -1, -1):
-        tf.reset_default_graph()
-        keep_prob = tf.placeholder(dtype=tf.float32)
-        test1 = all_data.iloc[:, [k]] # shape(105,1) m = 105
-        # train scaling #
-        mm1 = StandardScaler()
-        test1 = mm1.fit_transform(test1)
-        
-        ## split ## --> 시계열(시간순)
-        train_size = int(len(test1) * 0.8)
-        train_set = test1[:train_size, :] # shape(512, 5)
-        test_set = test1[train_size:, :] # test(220, 5)
-        
-        
-        # RNN data building #
-        def build(time_series, seq_length):
-            x_data = []
-            y_data = []
-            for i in range(0, len(time_series) - seq_length):
-                x_tmp = time_series[i: i + seq_length, :]
-                y_tmp = time_series[i + seq_length, [-1]]
-                x_data.append(x_tmp)
-                y_data.append(y_tmp)
-            return np.array(x_data), np.array(y_data)
-        
-        x_train, y_train = build(train_set, seq_length)
-        x_test, y_test = build(test_set, seq_length)
-        predict_x = test_set[-seq_length:].reshape(1, seq_length, 1)
-        
-        ## RNN building ##
-        # cell #
-        def lstm_cell(hidden_size):
-            cell = tf.nn.rnn_cell.LSTMCell(num_units=hidden_size, activation=tf.tanh)
-            return cell
-        
-        cell1 = rnn.DropoutWrapper(lstm_cell(50), input_keep_prob=keep_prob, output_keep_prob=keep_prob, seed=77)
-        cell2 = rnn.DropoutWrapper(lstm_cell(40), input_keep_prob=keep_prob, output_keep_prob=keep_prob, seed=77)
+for k in [273]:
+    tf.reset_default_graph()
+    keep_prob = tf.placeholder(dtype=tf.float32)
+    test1 = all_data.iloc[:, [k]] # shape(105,1) m = 105
+    # train scaling #
+    mm1 = StandardScaler()
+    test1 = mm1.fit_transform(test1)
+    
+    ## split ## --> 시계열(시간순)
+    train_size = int(len(test1) * 0.8)
+    train_set = test1[:train_size, :] # shape(512, 5)
+    test_set = test1[train_size:, :] # test(220, 5)
+    
+    
+    # RNN data building #
+    def build(time_series, seq_length):
+        x_data = []
+        y_data = []
+        for i in range(0, len(time_series) - seq_length):
+            x_tmp = time_series[i: i + seq_length, :]
+            y_tmp = time_series[i + seq_length, [-1]]
+            x_data.append(x_tmp)
+            y_data.append(y_tmp)
+        return np.array(x_data), np.array(y_data)
+    
+    x_train, y_train = build(train_set, seq_length)
+    x_test, y_test = build(test_set, seq_length)
+    predict_x = test_set[-seq_length:].reshape(1, seq_length, 1)
+    
+    ## RNN building ##
+    # cell #
+    def lstm_cell(hidden_size):
+        cell = tf.nn.rnn_cell.LSTMCell(num_units=hidden_size, activation=tf.tanh)
+        return cell
+    
+    cell1 = rnn.DropoutWrapper(lstm_cell(30), input_keep_prob=keep_prob, output_keep_prob=keep_prob, seed=77)
+    cell2 = rnn.DropoutWrapper(lstm_cell(20), input_keep_prob=keep_prob, output_keep_prob=keep_prob, seed=77)
 #        cell3 = rnn.DropoutWrapper(lstm_cell(10), input_keep_prob=keep_prob, output_keep_prob=keep_prob, seed=77)
 #        cell4 = rnn.DropoutWrapper(lstm_cell(25), input_keep_prob=keep_prob, output_keep_prob=keep_prob, seed=77)
-        #cell5 = rnn.DropoutWrapper(lstm_cell(), input_keep_prob=keep_prob, output_keep_prob=keep_prob, seed=77)
-        
-        #cell = rnn.BasicLSTMCell(num_units=hidden_size, state_is_tuple=True, activation=tf.tanh)
-        cell = rnn.MultiRNNCell([cell1, cell2], state_is_tuple=True) # dropout cell 5개
-        #
-        X = tf.placeholder(dtype=tf.float32, shape=[None, seq_length, data_dim])
-        y = tf.placeholder(dtype=tf.float32, shape=[None, 1])
-        #
-        ## 초기화 #
-        output, _state = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32) 
-        Y_pred = tf.contrib.layers.fully_connected(output[:, -1], output_dim, activation_fn=None) # last cell output --> 15일 뒤
-        
-    #    # config #
-    #    config = tf.ConfigProto(log_device_placement=True)
-    #    config.gpu_options.allow_growth = True
-        
-        
-        # cost #
-        cost = tf.reduce_sum(tf.square(Y_pred - y)) # sum of sq --> 수치 예측이기 때문에 sq loss가 필요 없다.
-        opt = tf.train.AdamOptimizer(learning_rate=learning_rate)
-        train = opt.minimize(cost)
-        
-        # MSE # --> mean squared error
-        targets= tf.placeholder(tf.float32, [None, 1])
-        predicts = tf.placeholder(tf.float32, [None, 1])
-        MSE = tf.sqrt(tf.reduce_mean(tf.square(predicts - targets)))
-        
-        ## session ##
-        # training#
-        sess = tf.Session()
-        sess.run(tf.global_variables_initializer())
-        for i in range(iteration):
-            cost_val, _, out= sess.run([cost, train, output], feed_dict={X: x_train, y: y_train, keep_prob:0.7})
+    #cell5 = rnn.DropoutWrapper(lstm_cell(), input_keep_prob=keep_prob, output_keep_prob=keep_prob, seed=77)
+    
+    #cell = rnn.BasicLSTMCell(num_units=hidden_size, state_is_tuple=True, activation=tf.tanh)
+    cell = rnn.MultiRNNCell([cell1, cell2], state_is_tuple=True) # dropout cell 5개
+    #
+    X = tf.placeholder(dtype=tf.float32, shape=[None, seq_length, data_dim])
+    y = tf.placeholder(dtype=tf.float32, shape=[None, 1])
+    #
+    ## 초기화 #
+    output, _state = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32) 
+    Y_pred = tf.contrib.layers.fully_connected(output[:, -1], output_dim, activation_fn=None) # last cell output --> 15일 뒤
+    
+#    # config #
+#    config = tf.ConfigProto(log_device_placement=True)
+#    config.gpu_options.allow_growth = True
+    
+    
+    # cost #
+    cost = tf.reduce_sum(tf.square(Y_pred - y)) # sum of sq --> 수치 예측이기 때문에 sq loss가 필요 없다.
+    opt = tf.train.AdamOptimizer(learning_rate=learning_rate)
+    train = opt.minimize(cost)
+    
+    # MSE # --> mean squared error
+    targets= tf.placeholder(tf.float32, [None, 1])
+    predicts = tf.placeholder(tf.float32, [None, 1])
+    MSE = tf.sqrt(tf.reduce_mean(tf.square(predicts - targets)))
+    
+    ## session ##
+    # training#
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    for i in range(iteration):
+        cost_val, _, out= sess.run([cost, train, output], feed_dict={X: x_train, y: y_train, keep_prob:1.0})
 #            if i % 1000 == 0:
 #                print(cost_val)
-        
-        # predict #
-        y_hat_train = sess.run(Y_pred, feed_dict={X: x_train, keep_prob:1.0})
-        y_hat = sess.run(Y_pred, feed_dict={X: x_test, keep_prob:1.0})
-        y_hat = mm1.inverse_transform(y_hat)
-        y_test = mm1.inverse_transform(y_test)
-        RMSE_train = sess.run(MSE, feed_dict={targets: y_train, predicts: y_hat_train, keep_prob:1.0})
-        RMSE = sess.run(MSE, feed_dict={targets: y_test, predicts: y_hat, keep_prob:1.0})
-        print("MSE_train: ", RMSE_train)
-        print("MSE: ", RMSE)
-        predict_hat = sess.run(Y_pred, feed_dict={X: predict_x, keep_prob:1.0})
-        
-        MSE_list.append(RMSE)
-        predict_list.append(mm1.inverse_transform(predict_hat)[0,0])
-        plt.figure()
-        plt.plot(y_train, 'r-')
-        plt.plot(y_hat_train, 'b-')
-        plt.show()
-        
-        plt.figure()
-        plt.plot(y_test, 'r-')
-        plt.plot(y_hat, 'b-')
-        plt.show()
-        sess.close()
-        
+    
+    # predict #
+    y_hat_train = sess.run(Y_pred, feed_dict={X: x_train, keep_prob:1.0})
+    y_hat = sess.run(Y_pred, feed_dict={X: x_test, keep_prob:1.0})
+#    y_hat = mm1.inverse_transform(y_hat)
+#    y_test = mm1.inverse_transform(y_test)
+    RMSE_train = sess.run(MSE, feed_dict={targets: y_train, predicts: y_hat_train, keep_prob:1.0})
+    RMSE = sess.run(MSE, feed_dict={targets: y_test, predicts: y_hat, keep_prob:1.0})
+    print("RMSE_train: ", RMSE_train)
+    print("RMSE: ", RMSE)
+    predict_hat = sess.run(Y_pred, feed_dict={X: predict_x, keep_prob:1.0})
+    
+    MSE_list.append(RMSE)
+    predict_list.append(mm1.inverse_transform(predict_hat)[0,0])
+    plt.figure(figsize=(15,8))
+    plt.plot(y_train, 'r-')
+    plt.plot(y_hat_train, 'b-')
+    plt.xlabel("Time")
+    plt.ylabel("Population")
+    plt.show()
+    
+    plt.figure(figsize=(15,8))
+    plt.plot(y_test, 'r-')
+    plt.plot(y_hat, 'b-')
+    plt.xlabel("Time")
+    plt.ylabel("Population")
+    plt.show()
+    sess.close()
 
 
